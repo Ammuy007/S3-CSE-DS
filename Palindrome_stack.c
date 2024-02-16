9e5a265caa449acb545fdfb2661f50a292a0b45d
Ammuy007 <149078646+Ammuy007@users.noreply.github.com>
2024-02-16 05:53:56 -0800

    Create Palindrome_stack.c

---
+++ Palindrome_stack.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define N 30
+#include <string.h>
+char s[N];
+int top=-1;
+void push(char a){
+    if(top==N-1){
+        printf("Stack full\n");
+    }
+    else{
+        s[++top]=a;
+    }
+}
+char pop(){
+    char item;
+    if(top==-1){
+        printf("Stack empty\n");
+        //return '\0';
+    }
+    else{
+        item=s[top--];
+        //printf("%c",item);
+        return item;
+    }
+}
+
+void main(){
+    char str[N],rev[N];
+    int i,j,k,flag=1;
+    printf("Enter the string\n");
+    scanf("%s",str);
+    for(i=0;str[i]!='\0';i++){
+        push(str[i]);
+    }
+    i=0;
+    while(top>=0){
+    if(pop()!=str[i]){
+        flag=0;
+        break;
+    }
+    i++;
+    }
+    if(flag==0){
+        printf("Not palindrome");
+    }
+    if(flag==1){
+        printf("Palindrome");
+    }
+}
